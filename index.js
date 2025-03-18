092ebc12ef76f73e19e177a402d0ba90db03058c
arbaz <khalidarbaz27@gmail.com>
2025-03-19 02:32:59 +0500

    a

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import Form from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Form />
-);
+export default Form;
